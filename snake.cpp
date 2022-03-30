62b55cd0ebd5c0b29711e3e83d9f84952d696ab8
seven-ronaldo <hao7ehoathanh@gmail.com>
2022-03-30 10:03:10 +0700

    first commit

--- snake.cpp
+++ snake.cpp
@@ -16,6 +16,94 @@ void addscore(){
  	gotoxy(79,13);
  	printf("%d",score%10);
 }
+void ingame(){
+	int key;
+	int i;
+	initgame();
+ 	vekhung();
+ 	drawscore();
+ 	state = INGAME;
+ 	SetColor(13);
+ 	gotoxy(getx(fruit),gety(fruit));
+ 	printf("O");
+ 	SetColor(12);
+ 	gotoxy(getx(character[0]),gety(character[0]));
+	printf("#");
+	i = 1;
+	SetColor(2);
+ 	for(i;i<length;i++){
+		gotoxy(getx(character[i]),gety(character[i]));
+		printf("o");}
+	while(state == INGAME){
+	if(kbhit()){
+			key = getch();
+			switch (key) {
+				//nut len
+				case 72:
+					if(vy == 0){
+						vx = 0;
+						vy = -1;}
+					break;
+					//nut xuong
+				case 80:
+					if(vy == 0){
+						vx = 0;
+						vy = 1;}
+					break;
+					//nut trai
+				case 75:
+					if(vx == 0){
+						vy = 0;
+						vx = -1;}
+					break;
+					// nut phai
+				case 77:
+					if(vx == 0){
+						vy = 0;
+						vx = 1;}
+					break;		}						     
+	}	
+	if((clock()-t)>=s_e){
+			if(character[length-1]!=0){			
+			gotoxy(getx(character[length-1]),gety(character[length-1]));
+			printf(" ");}
+			i =length-1;
+			for(i;i>0;i--){
+			  character[i]=character[i-1];}
+			SetColor(2);
+			gotoxy(getx(character[0]),gety(character[0]));
+			printf("o");
+		    character[0] += vx*100;
+		    character[0] += vy;	
+		    if(character[0] == fruit){
+		    	length+= 1;
+		    	score += (speed/10 + 5);
+		    	addscore();
+		    	creatfruit();
+		    	SetColor(13);
+		    	gotoxy(getx(fruit),gety(fruit));
+ 	            printf("O");}
+			if((vx>0)&&(getx(character[0])==78)){
+				character[0]-=7700;}    
+			if((vx<0)&&(getx(character[0])==0)){
+				character[0]+=7700;}  
+			if((vy>0)&&(gety(character[0])==24)){
+				character[0]-=23;}   
+			if((vy<0)&&(gety(character[0])==0)){
+				character[0]+=23;}   
+			SetColor(12);
+			gotoxy(getx(character[0]),gety(character[0]));
+				printf("#");
+			i = 1;
+			for(i;i<length;i++){
+				if(character[i]==character[0]){
+					system("cls");					
+					state = GAMEOVER;}
+			}  
+		   t = clock();  		    
+		}	
+	}	
+}
 void ingame()
 int main(){
 	state = MENU;
